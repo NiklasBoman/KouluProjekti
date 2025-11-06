9bc6428918293cbcc3fd93781dd3b4635e18071c
NiklasBoman <nikke.boman@gmail.com>
2025-11-06 12:13:50 +0200

    Add files via upload
    Koodi joka yhdistää tietokantaan.

---
+++ sql/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "213572";
+$password = "G96XnfMp7rci9UAA";
+$database = "213572";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if($conn->connect_error){
+    die("Yhteys epäonnistui: " . $conn->connect_error);
+}
+?php
