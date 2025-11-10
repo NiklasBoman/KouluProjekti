0220629ae04bb56cb7f3d85faec323d705253b2e
sgee-del <sakke3p@gmail.com>
2025-11-10 13:05:16 +0200

    Korjattu polut

--- frontend/login.php
+++ frontend/login.php
@@ -60,6 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="../public/assets/css/logintyyli.css">
 </head>
 <body>
 <h1>Kirjautuminen</h1>
--- frontend/register.php
+++ frontend/register.php
@@ -59,6 +59,7 @@ if (isset($_POST['rekisteroi'])) {
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="../public/assets/css/logintyyli.css">
 </head>
 <body>
 <h1>Rekisteröityminen</h1>
