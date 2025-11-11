2618dd4a39e130a0dc0f81a5de5a256bdb80ed97
NiklasMonkkonen <niklas.monkkonen7@gmail.com>
2025-11-11 13:27:36 +0200

    Update settings.php

--- frontend/settings.php
+++ frontend/settings.php
@@ -13,4 +13,48 @@ if (!isset($_SESSION['KayttajaID'])) {
 // Määritellään $kayttaja-muuttuja nimen näyttämistä varten
 $kayttaja = $_SESSION['Nimi'] ?? 'Käyttäjä';
 
+$profiilikuva = $_SESSION['Profiilikuva'] ?? '../public/assets/images/profile_placeholder.svg';
 ?>
+<!doctype html>
+<html>
+<head>
+<title>settings</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="../public/assets/css/style.css">
+
+</head>
+<body>
+
+
+<div class="keskitin">
+    <div class="container">
+<h1>Asetukset</h1>
+    <div class="settings-profile">
+            <img src="<?php echo htmlspecialchars($profiilikuva); ?>" 
+                 alt="Profiilikuva" class="profile-pic">
+                 <button  id="kvaihto">Vaihda kuva</button>
+        </div>
+        
+        <label>Nimi</label>
+        <input type="text">
+        <div class="button2">
+        <button  id="svaihto">Salasanan vaihto</button>
+        </div>
+
+        <label>Sähköposti</label>
+        <input type="email">
+        <div class="button2">
+        <button  id="svaihto">Salasanan vaihto</button>
+        </div>
+
+        <label>Salasana</label>
+        <input type="password">
+        <div class="button2">
+        <button  id="svaihto">Salasanan vaihto</button>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<body>
