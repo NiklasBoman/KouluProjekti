c0d217c78220ec8cede104cb7ffce890562e0fa9
sgee-del <sakke3p@gmail.com>
2025-11-11 12:45:37 +0200

    favicon

--- frontend/header_footer/header_frontend.php
+++ frontend/header_footer/header_frontend.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Luokkavaraus järjestelmä</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+    <!-- Favicon-linkit -->
+    <link rel="icon" href="../public/assets/images/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="../public/assets/images/apple-touch-icon.png">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../public/assets/css/style_frontend.css">
 </head>
